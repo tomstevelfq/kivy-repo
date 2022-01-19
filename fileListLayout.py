02953e73d672991ad060e34ce038f80472a90d5f
fqman <tomsetvelfq@gmail.com>
2022-01-19 21:09:41 +0800

    accept_file_and_dir_name

--- fileListLayout.py
+++ fileListLayout.py
@@ -6,7 +6,7 @@ from fileicon import fileIcon,dirIcon
 from kivy.uix.label import Label
 import os
 
-for root,dirs,files in os.walk('.'):
+for root,dirs,files in os.walk(os.getcwd()):
     break
 
 class FileApp(App):
@@ -17,13 +17,13 @@ class scrollFileView(ScrollView):
     def __init__(self,root,dirs,files,**kwargs):
         print(root,dirs,files)
         super().__init__(**kwargs)
-        self.st=StackLayout(size_hint=(1,None))
-        for i in range(50):
-            self.st.add_widget(fileIcon(size_hint=(0.125,None)))
-        for i in range(50):
-            self.st.add_widget(dirIcon(size_hint=(0.125,None)))
+        self.st=StackLayout(size_hint=(1,None),spacing=10)
+        for i in dirs:
+            self.st.add_widget(dirIcon(fname=i,size_hint=(0.125,None)))
+        for i in files:
+            self.st.add_widget(fileIcon(fname=i,size_hint=(0.125,None)))
         self.st.bind(minimum_height=self.st.setter('height'))
         self.size_hint=(1,1)
         self.add_widget(self.st)
 
-FileApp().run()
+FileApp(title=root).run()
