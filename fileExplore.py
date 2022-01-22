63e7aa796419c80622f60d4dcb8e0045ac6e08da
fqman <tomsetvelfq@gmail.com>
2022-01-22 21:17:40 +0800

    navibar

--- fileExplore.py
+++ fileExplore.py
@@ -1,56 +1,19 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from fileNaviBar import fileNaviBar
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.stacklayout import StackLayout
-from kivy.graphics import Line,Color
-from kivy.uix.button import Button
-from kivy.uix.label import Label
+from kivy.graphics import Color,Rectangle
 
-class MyLabel(Label):
+class mainInterface(StackLayout):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         with self.canvas:
-            Color(1,0,1,1)
-            self.bor=Line(width=3,rectangle=(self.x,self.y,self.width,self.height))
-        self.bind(size=self.update,pos=self.update) 
+            Color(0.1484,0.1680,0.1836,1)
+            self.rt=Rectangle()
+        self.bind(size=self.update,pos=self.update)
+        self.add_widget(fileNaviBar(size_hint=(1,None),height=40))
     def update(self,*args):
-        self.bor.rectangle=(self.x,self.y,self.width,self.height)
-
-class tlay(FloatLayout):
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
-        with self.canvas:
-            Color(1,0,1,1)
-            self.bor=Line(width=3,rectangle=(self.x,self.y,self.width,self.height)) 
-        self.bind(size=self.update_rec,pos=self.update_rec)
-        # self.lab=Label(text='haha')
-        # self.add_widget(self.lab)
-        self.lab1=MyLabel(text='haha',size_hint=(0.2,1),pos_hint={'x':0})
-        self.lab2=MyLabel(text='haha',size_hint=(0.8,1),pos_hint={'x':0.2})
-        self.add_widget(self.lab1)
-        self.add_widget(self.lab2)
-    def update_rec(self,*args):
-        self.bor.rectangle=(self.x,self.y,self.width,self.height)
-        self.lab1.pos=self.pos
-        self.lab2.pos=self.pos
-
-class mainInterface(BoxLayout):
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
-        #fnavbar=fileNaviBar(size_hint=(1,None),height=100)
-        self.orientation='vertical'
-        self.add_widget(tlay(size_hint=(1,None),height=100))
-        self.add_widget(tlay(size_hint=(1,None),height=100))
-        self.bind(height=self.sety)
-        # self.add_widget(tlay(size_hint=(1,None),height=100))
-        # self.add_widget(tlay(size_hint=(1,None),height=100))
-        #self.add_widget(tlay(size_hint=(1,None),height=500))
-
-    def sety(self,*args):
-        self.y=self.height-200
-        
-
+        self.rt.pos=self.pos
+        self.rt.size=self.size
 
 class fileApp(App):
     def build(self):
--- fileNaviBar.py
+++ fileNaviBar.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.label import Label
 from kivy.metrics import Metrics
 from kivy.graphics import Line,Color
@@ -14,26 +15,21 @@ class fileNaviBar(FloatLayout):
     zoom_attr=1
     def __init__(self,zoom_attr=1,**kwargs):
         super(fileNaviBar,self).__init__(**kwargs)
-        with self.canvas:
-           Color(1,0,1,1)
-           self.bor=Line(width=3,rectangle=(self.x,self.y,0,0)) 
-        self.bind(size=self.update_border,pos=self.update_border)
         self.apply_zoom(zoom_attr)
-        img=MyImage(source='./pics/option.png',size_hint=(None,None),size=(self.length,self.length))
+        self.img=MyImage(source='./pics/option.png',size_hint=(None,None),size=(self.length,self.length))
         self.orientation='horizontal'
-        self.add_widget(img)
+        self.add_widget(self.img)
         self.lab=Label(padding=[10,0],halign='left',valign='center',text='hello world',font_size=self.fsize,size_hint=(None,None),height=self.length)
         self.add_widget(self.lab)
         with self.lab.canvas:
             Color(1,1,1,1)
             self.line=Line(width=1,rectangle=(self.lab.x,self.lab.y,0,0))
-        self.bind(size=self.update_lab,pos=self.update_lab)
-        self.lab.bind(size=self.update_lab,pos=self.update_lab)
         self.serimg=MyImage(source='./pics/search.png',size_hint=(None,None),size=(self.length,self.length))
         self.opt1img=MyImage(source='./pics/option1.png',size_hint=(None,None),size=(self.length,self.length))
         self.add_widget(self.serimg)
         self.add_widget(self.opt1img)
-        self.bind(size=self.updateimg)
+        self.bind(size=self.update,pos=self.update)
+        self.lab.bind(size=self.update,pos=self.update)
     
     def apply_zoom(self,zoom_attr):
         self.zoom_attr=zoom_attr
@@ -41,23 +37,21 @@ class fileNaviBar(FloatLayout):
         self.fsize*=zoom_attr
         self.gap1*=zoom_attr
         self.gap2*=zoom_attr
-    def update_lab(self,*args,**kwargs):
-        self.lab.width=self.width-(130*self.zoom_attr)
-        self.lab.text_size=self.lab.size
-        self.line.rectangle=(self.lab.x,self.lab.y,self.lab.width,self.lab.height)
-    
-    def updateimg(self,*args,**kwargs):
+
+    def update(self,*args):
         self.serimg.pos=(self.width-self.gap1,self.y)
         self.opt1img.pos=(self.width-self.gap2,self.y)
         self.lab.pos=(self.gap2,self.y)
-    
-    def update_border(self,size,*args):
-        self.bor.rectangle=(self.x,self.y,self.width,self.height)
-
+        self.img.pos=(self.x,self.y)
+        self.lab.width=self.width-(130*self.zoom_attr)
+        self.lab.text_size=self.lab.size
+        self.line.rectangle=(self.lab.x,self.lab.y,self.lab.width,self.lab.height)
 
 class test(App):
     def build(self):
-        return fileNaviBar(zoom_attr=1)
+        st=StackLayout()
+        st.add_widget(fileNaviBar(size_hint=(1,None),height=40))
+        return st
 
 if __name__=='__main__':
     test().run()
